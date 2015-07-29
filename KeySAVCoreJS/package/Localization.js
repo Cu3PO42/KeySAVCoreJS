8dc1b35d852326a7de105a391f722c18a8d06375
Copper Phosphate <cu3po42@gmail.com>
2015-07-29 18:50:57 +0200

    Add localization implementation.

---
+++ KeySAVCoreJS/package/Localization.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+    names = {};
+
+var langs = ["de", "en", "es", "fr", "it", "ja", "ko"];
+var files = ["abilities", "forms", "items", "moves", "natures", "species", "types"];
+
+for (var i = 0; i < langs.length; ++i) {
+    var lang = names[langs[i]] = {};
+    for (var j = 0; j < files.length; ++j) {
+        lang[files[j]] = fs.readFileSync(__dirname + "/localization/" + langs[i] + "/" + files[j] + ".txt", {encoding: "utf-8"}).split("\n");
+    }
+}
+
+module.exports = names;
