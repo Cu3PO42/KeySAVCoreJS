17cfb80359a3808d5477207eed4fb4de61ea9747
Copper Phosphate <cu3po42@gmail.com>
2015-08-12 18:32:06 +0200

    Add level calculation to main export

--- KeySAVCoreJS/package/index.js
+++ KeySAVCoreJS/package/index.js
@@ -1,11 +1,13 @@
 var core = require('./KeySAVCore');
 var KeyStore = require('./KeyStore');
 var Localization = require("./Localization")
+var calculateLevel = require("./level")
 
 module.exports = {
     Core: core,
     Extensions: {
         KeyStore: KeyStore,
-        Localization: Localization
+        Localization: Localization,
+        calculateLevel: calculateLevel
     }
 };
