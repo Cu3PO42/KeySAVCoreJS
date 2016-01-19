45f46068200e4db2bc9582be5e6b6eac23ec51dc
Copper Phosphate <cu3po42@gmail.com>
2016-01-19 23:12:43 +0100

    Move files and remove C# source

--- KeySAVCoreJS/scripts/KeySAVCore.d.ts
+++
@@ -1,6 +0,0 @@
-import KeySAVCore = require("./KeySAVCoreJS");
-
-export import Core = require("./KeySAVCoreJS");
-export import Localization = require("./localization");
-export import Calculator = require("./calculator");
-export import KeyStore = require("./key-store")
