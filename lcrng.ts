38bed7ead3084bd9da986a61b1642ff7adcc4c0e
Copper Phosphate <cu3po42@gmail.com>
2016-11-17 15:08:47 +0100

    Remove dependency on cuint

--- lcrng.ts
+++ lcrng.ts
@@ -1,10 +1,17 @@
-/// <reference path="typings/cuint/cuint.d.ts"/>
-
-import * as cuint from "cuint";
-
-var mul_const = cuint.UINT32(1103515245);
-var add_const = cuint.UINT32(24691);
+const add_const_lo = 0x6073;
+const mul_const_lo = 0x4E6D;
+const mul_const_hi = 0x41C6;
 
 export function next(seed) {
-    return cuint.UINT32(seed).multiply(mul_const).add(add_const).toNumber() >>> 0;
+    let seed_lo = seed & 0xFFFF;
+    let seed_hi = seed >>> 0x10;
+
+    let res_lo = seed_lo * mul_const_lo;
+    let res_hi = ((((res_lo >>> 0x10) + seed_hi * mul_const_lo) & 0xFFFF) + seed_lo * mul_const_hi) & 0xFFFF;
+    res_lo &= 0xFFFF;
+
+    res_lo += add_const_lo;
+    res_hi += res_lo >>> 0x10;
+
+    return ((res_lo & 0xFFFF) | ((res_hi & 0xFFFF) << 0x10)) >>> 0;
 }
