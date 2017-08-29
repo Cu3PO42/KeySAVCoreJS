16b65270a0237e26ce9970982a56a676595853c8
Copper Phosphate <cu3po42@gmail.com>
2017-08-29 19:53:08 +0200

    Document Pk6

--- pk6.ts
+++ pk6.ts
@@ -1,10 +1,21 @@
 import * as util from "./util";
 import PkBase, { registerPkmImpl } from "./pkbase";
 
+/**2yj
+ * The generation 6 specialization of a Pokémon.
+ */
 export default class Pk6 extends PkBase {
     // Uints
     public trainingBagHitsRemaining: number;
 
+    /**
+     * Construct a new Pk6 representation.
+     * 
+     * @param pkx The raw Pk6 data.
+     * @param box The box the Pokémon is located in.
+     * @param slot The slot in the box the Pokémon is located in.
+     * @param isGhost True if the Pokémon might be an artifact of bad decryption
+     */
     constructor(pkx: Uint8Array, box: number, slot: number, isGhost: boolean) {
         super(pkx, box, slot, isGhost);
 
