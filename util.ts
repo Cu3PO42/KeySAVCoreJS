f4edf668be7a076953fc14a2e3e6a0506810ca55
Copper Phosphate <cu3po42@gmail.com>
2017-08-29 11:13:43 +0200

    Fix compile errors in TS 2.4

--- util.ts
+++ util.ts
@@ -342,14 +342,14 @@ export function sequenceEqual(src1: Uint8Array, b, c?, d?, e?): boolean {
     return true;
 }
 
-export function promisify<T>(fn: (cb: (err: Error, res: T) => void) => void): () => Promise<T>;
-export function promisify<T, A1>(fn: (arg1: A1, cb: (err: Error, res: T) => void) => void): (arg1: A1) => Promise<T>;
-export function promisify<T, A1, A2>(fn: (arg1: A1, arg2: A2, cb: (err: Error, res: T) => void) => void): (arg1: A1, arg2: A2) => Promise<T>;
-export function promisify<T, A1, A2, A3>(fn: (arg1: A1, arg2: A2, arg3: A3, cb: (err: Error, res: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3) => Promise<T>;
-export function promisify<T, A1, A2, A3, A4>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, cb: (err: Error, res: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
-export function promisify<T, A1, A2, A3, A4, A5>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, cb: (err: Error, res: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => Promise<T>;
-export function promisify<T, A1, A2, A3, A4, A5, A6>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6, cb: (err: Error, res: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6) => Promise<T>;
-export function promisify<T, A1, A2, A3, A4, A5, A6, A7>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6, arg7: A7, cb: (err: Error, res: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6, arg7: A7) => Promise<T>;
+export function promisify<T>(fn: (cb: (err: Error, res?: T) => void) => void): () => Promise<T>;
+export function promisify<T, A1>(fn: (arg1: A1, cb: (err: Error, res?: T) => void) => void): (arg1: A1) => Promise<T>;
+export function promisify<T, A1, A2>(fn: (arg1: A1, arg2: A2, cb: (err: Error, res?: T) => void) => void): (arg1: A1, arg2: A2) => Promise<T>;
+export function promisify<T, A1, A2, A3>(fn: (arg1: A1, arg2: A2, arg3: A3, cb: (err: Error, res?: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3) => Promise<T>;
+export function promisify<T, A1, A2, A3, A4>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, cb: (err: Error, res?: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
+export function promisify<T, A1, A2, A3, A4, A5>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, cb: (err: Error, res?: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => Promise<T>;
+export function promisify<T, A1, A2, A3, A4, A5, A6>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6, cb: (err: Error, res?: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6) => Promise<T>;
+export function promisify<T, A1, A2, A3, A4, A5, A6, A7>(fn: (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6, arg7: A7, cb: (err: Error, res?: T) => void) => void): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5, arg6: A6, arg7: A7) => Promise<T>;
 export function promisify(fn: Function) {
     return function(...args: any[]) {
         return new Promise(function(resolve, reject) {
