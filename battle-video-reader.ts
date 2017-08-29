0ce8b04b108ffb17075972c32ee23fbc6aedd656
Copper Phosphate <cu3po42@gmail.com>
2017-08-29 16:17:33 +0200

    Document BattleVideoReader

--- battle-video-reader.ts
+++ battle-video-reader.ts
@@ -2,19 +2,40 @@ import PkBase from "./pkbase";
 import * as util from "./util";
 import BattleVideoKey from "./battle-video-key";
 
+/**
+ * A class to allow accessing the teams stored in a battle video with the appropriate key.
+ */
 export default class BattleVideoReader {
+    /**
+     * The generation this battle video is from.
+     */
     public generation: number;
     private _offsets: { partyOffsets: number[] };
 
+    /**
+     * The raw battle video data this reader is accessing.
+     */
     public get video() {
         return this._video;
     }
 
+    /**
+     * Create a new reader for a battle video with the provided key.
+     * 
+     * @param _video The battle video to be accessed
+     * @param key The key for the relevant battle video slot
+     */
     constructor(private _video: Uint8Array, private key: BattleVideoKey) {
         this.generation = BattleVideoReader.getGeneration(_video);
         this._offsets = BattleVideoReader.getOffsets(this.generation);
     }
 
+    /**
+     * Get the generation a battle video is from.
+     * 
+     * @param video The battle video to check
+     * @return The generation
+     */
     public static getGeneration(video: Uint8Array) {
         switch (video.length) {
             case 0x6e60:
@@ -26,6 +47,12 @@ export default class BattleVideoReader {
         }
     }
 
+    /**
+     * Get the offsets the parties are stored at in a battle video in a given generation.
+     * 
+     * @param generation The generation for which to retrieve the offsets
+     * @return The relevant offsets
+     */
     public static getOffsets(generation: number) {
         return {
             6: {
@@ -37,10 +64,20 @@ export default class BattleVideoReader {
         }[generation];
     }
 
+    /**
+     * Get an array of flags indicating which parties are readable with the given key.
+     */
     get workingKeys() {
         return this.key.workingKeys;
     }
 
+    /**
+     * Read any Pokémon from the battle video. If the slot cannot be decrypted or there is no Pokémon there,
+     * return undefined.
+     * 
+     * @param slot The Pokémons position in the team
+     * @param team The number of the team the Pokémon is in
+     */
     getPkx(slot: number, team: number): PkBase {
         const ekx = util.xor(this._video, this._offsets.partyOffsets[team] + 260 * slot, this.key.teamKeys[team], 260 * slot, 260);
         const pkx = PkBase.decrypt(ekx);
@@ -51,6 +88,9 @@ export default class BattleVideoReader {
         return PkBase.makePkm(pkx, this.key.generation, -1, slot, false);
     }
 
+    /**
+     * Get all Pokémon that can be decrypted from this battle video.
+     */
     getAllPkx() {
         const res = [];
         for (let i = 0; i < this.key.teamKeys.length; ++i) {
