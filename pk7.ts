27d02a7232384dfaaec7b4d14c42fc872b3733dc
Copper Phosphate <cu3po42@gmail.com>
2017-08-29 19:53:18 +0200

    Document Pk7

--- pk7.ts
+++ pk7.ts
@@ -1,9 +1,23 @@
 import PkBase, { registerPkmImpl } from "./pkbase";
 import * as util from "./util";
 
+/**
+ * The list of all form names in generation 6, ordered by species, then form ID.
+ */
 export default class Pk7 extends PkBase {
+    /**
+     * The TID that is shown in Generation 7 games.
+     */
     public tid7: number;
 
+    /**
+     * Construct a new Pk7 representation.
+     * 
+     * @param pkx The raw Pk7 data.
+     * @param box The box the Pokémon is located in.
+     * @param slot The slot in the box the Pokémon is located in.
+     * @param isGhost True if the Pokémon might be an artifact of bad decryption
+     */
     constructor(pkx: Uint8Array, box: number, slot: number, isGhost: boolean) {
         super(pkx, box, slot, isGhost);
 
