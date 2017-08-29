b928e359e5bedb4966d48d402c0eb69dd2b6a2e8
Copper Phosphate <cu3po42@gmail.com>
2017-08-29 17:03:10 +0200

    Document KeyStore and related methods

--- key-store.ts
+++ key-store.ts
@@ -2,27 +2,91 @@ import SaveKey from "./save-key";
 import BattleVideoKey from "./battle-video-key";
 import { createDataView, getStampSav, getStampBv } from "./util";
 
+/**
+ * An object that stores any amount of keys for battle videos and saves and can be used to retrieve them.
+ */
 export interface KeyStore {
+    /**
+     * Get a promise for save key for the given stamp if available, undefined otherwise.
+     * 
+     * @param stamp The stamp that identifies the save
+     * @return A promise for the save key, if available
+     */
     getSaveKey(stamp: string): Promise<SaveKey>;
+
+    /**
+     * Get a promise for a battle video key for the given stamp if available, undefined otherwise.
+     * 
+     * @param stamp The stamp that identifies the battle video
+     * @return A promise for the battle video key, if available
+     */
     getBvKey(stamp: string): Promise<BattleVideoKey>;
+
+    /**
+     * Set a save key. The returned promise is resolved upon completion.
+     * 
+     * @param key The save key to set
+     * @return A promise that resolves when the operation is complete
+     */
     setSaveKey(key: SaveKey): Promise<void>;
+
+    /**
+     * Set a given save key if a key for the save is not already available. Otherwise merge the given key with the
+     * existing one.
+     * 
+     * @param key The save key to set or merge
+     * @return A promise that resolves when the operation is complete
+     */
     setOrMergeSaveKey(key: SaveKey): Promise<void>;
+
+    /**
+     * Set a battle video key. The returned promise is resolved upon completion.
+     * 
+     * @param key The battle video key to set
+     * @return A promise that resolves when the operation is complete
+     */
     setBvKey(key: BattleVideoKey): Promise<void>;
+
+    /**
+     * Set a given battle video key if a key for the save is not already available. Otherwise merge the given key with
+     * the existing one.
+     * 
+     * @param key The battle video key to set or merge
+     * @return A promise that resolves when the operation is complete
+     */
     setOrMergeBvKey(key: BattleVideoKey): Promise<void>;
 }
 
 var currentKeyStore: KeyStore = undefined;
 
+/**
+ * Set the global key store. It is used by utility functions to store and retrieve keys for provided saves and
+ * battle videos.
+ * 
+ * @param store The key store to be used globally
+ */
 export function setKeyStore(store: KeyStore): void {
     currentKeyStore = store;
 }
 
+/**
+ * Return the global key store.
+ * 
+ * @return The global key store.
+ */
 export function getKeyStore(): KeyStore {
     return currentKeyStore;
 }
 
 export default KeyStore;
 
+/**
+ * Extract the stamp and key variation (save or battle video) from the first 0x18 bytes of a key and the file size.
+ * 
+ * @param arr The first 0x18 byteo of the key
+ * @param size The complete size of the key file
+ * @return an object containing the stamp for the key and the kind: 0 for saves, 1 for battle videos
+ */
 export function getStampAndKindFromKey(arr: Uint8Array, size: number): { stamp: string, kind: number } {
     const dataView = createDataView(arr);
     if (dataView.getUint32(0x10, true) === 0xCAFEBABE) {
