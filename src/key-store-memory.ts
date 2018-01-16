dc86ee45175270acf3084245f6533f2616761861
Cu3PO42 <cu3po42@gmail.com>
2018-01-16 12:46:11 +0100

    Implement missing functionality in KeyStoreMemory

--- src/key-store-memory.ts
+++ src/key-store-memory.ts
@@ -1,7 +1,8 @@
 import SaveKey from "./save-key";
 import BattleVideoKey from "./battle-video-key";
+import { KeyStore } from "./key-store";
 
-export default class KeyStoreMemory {
+export default class KeyStoreMemory implements KeyStore {
   private saveKeys: { [stamp: string]: SaveKey } = {};
   private bvKeys: { [stamp: string]: BattleVideoKey } = {};
 
@@ -15,11 +16,29 @@ export default class KeyStoreMemory {
     return Promise.reject({ name: "NoKeyAvailableError" });
   }
 
-  setSaveKey(key) {
+  setSaveKey(key: SaveKey) {
     this.saveKeys[key.stamp] = key;
+    return Promise.resolve();
+  }
+
+  setOrMergeSaveKey(key: SaveKey) {
+    if (this.saveKeys[key.stamp])
+      this.saveKeys[key.stamp].mergeKey(key);
+    else
+      this.saveKeys[key.stamp] = key;
+    return Promise.resolve();
   }
 
   setBvKey(key) {
     this.bvKeys[key.stamp] = key;
+    return Promise.resolve();
+  }
+
+  setOrMergeBvKey(key: BattleVideoKey) {
+    if (this.bvKeys[key.stamp])
+      this.bvKeys[key.stamp].mergeKey(key);
+    else
+      this.bvKeys[key.stamp] = key;
+    return Promise.resolve();
   }
 }
